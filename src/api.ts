0da4d49ca6ca993d9d5eec6ff494169b6bf32014
Omari <othompsonedwards@gmail.com>
2025-01-20 17:52:28 +0000

    Add typing to server fetch function

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,17 @@
 import { BASE_URL, API_URL } from './consts';
 import { GetServersResponseSchema } from './schemas';
+import { GameServer } from './types';
 
-export async function getServers() {
+export async function getServers(): Promise<
+    | {
+          success: true;
+          data: GameServer[];
+      }
+    | {
+          success: false;
+          error: string;
+      }
+> {
     try {
         const response = await fetch(BASE_URL + API_URL).then((res) =>
             res.json()
@@ -11,6 +21,6 @@ export async function getServers() {
         return { success: true, data };
     } catch (e) {
         console.error(e);
-        return { success: false, error: e };
+        return { success: false, error: e as string };
     }
 }
